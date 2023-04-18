2236786c3e42eafe865b3725dde555de59c86a46
Sidnei <sidnei.teixeira@outlook.com>
2023-04-18 16:47:18 -0300

    Mysql + JPA + Test

---
+++ src/main/java/br/com/fiap/abctechapi/service/AssistanceService.java
@@ -0,0 +1,10 @@
+package br.com.fiap.abctechapi.service;
+
+
+import br.com.fiap.abctechapi.entity.Assistance;
+
+import java.util.List;
+
+public interface AssistanceService {
+    List<Assistance> getAssist();
+}
